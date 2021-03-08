2f50db3cc42a7447d4b2330a856557e23eca0a16
Siggi <siggisim@gmail.com>
2021-03-08 13:03:47 -0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,9 @@
+const baseUrl = "/docs/";
 module.exports = {
   title: 'BuildBuddy',
   tagline: 'BuildBuddy provides enterprise features for Bazel — the open source build system that allows you to build and test software 10x faster.',
   url: 'https://buildbuddy.io',
-  baseUrl: '/docs/',
+  baseUrl: baseUrl,
   favicon: 'img/favicon_black.svg',
   organizationName: 'buildbuddy-io', // Usually your GitHub org/user name.
   projectName: 'buildbuddy', // Usually your repo name.
@@ -66,8 +67,7 @@ module.exports = {
         {
           items: [
             {
-              // TODO make this relative to base url.
-              html: `<a href="/"><img src="/img/logo_white.svg" class="footer-logo" /></a>`,
+              html: `<a href="/"><img src="${baseUrl}img/logo_white.svg" class="footer-logo" /></a>`,
             }
 
           ],
